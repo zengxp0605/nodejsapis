5b080b5af66f1f73802f86595756396e1be72e26
zengxp@pa5 <zengxp@pa5.com>
2016-11-03 21:43:17 +0800

    update

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var port = process.env.PORT || 5000;
 var data = {
 	key: 'value', 
 	hello: 'world',
-	arr: _.range(10),
+	arr: _.range(5),
+	database_url: '' + process.env.DATABASE_URL,
+	
 };
 
 app.get('/', function(req, res){
@@ -17,6 +19,53 @@ app.get('/', function(req, res){
 	res.end();
 });
 
+app.get('/test', function(req, res){
+	console.log('test page');
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write("<h2>Hello Test</h2>");
+	res.end();
+});
+
+app.get('/db', function(req, res){
+try{
+	
+	var pg = require('pg');
+	
+	console.log('test db');
+
+	pg.defaults.ssl = true;
+	pg.connect(process.env.DATABASE_URL, function(err, client) {
+	  if (err){
+		 //throw err;
+		 console.log('Error---> ' + err);
+		 res.writeHead(200, {"Content-Type": "text/html"});
+		 res.write('Error---> ' + err);
+		 res.end();
+		 return;
+	  }  
+	  console.log('Connected to postgres! Getting schemas...');
+
+	  client
+		.query('SELECT table_schema,table_name FROM information_schema.tables;')
+		.on('row', function(row) {
+		    res.writeHead(200, {"Content-Type": "text/html"});
+			res.write(JSON.stringify(row));
+			console.log(JSON.stringify(row));
+			res.end();
+		});
+	});
+}catch(err){
+	res.writeHead(200, {"Content-Type": "text/html"});
+	res.write(JSON.stringify(err));
+	res.end();
+}
+
+});
+
 server.listen(port, function(){
 	console.log('socket on *:' + port)
+});
+
+process.on('uncaughtException', function(error) {
+    console.error('Caught exception: ' + error);
 });
