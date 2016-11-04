9c13195b85a4e23e01a24c0b3e2997e7456f866f
zengxp@pa5 <zengxp@pa5.com>
2016-11-04 11:45:12 +0800

    update

--- app.js
+++ app.js
@@ -4,126 +4,114 @@ var _ = require('underscore');
 var app = express();
 var server = require('http').Server(app);
 var port = process.env.PORT || 5000;
+var databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:123456@localhost:5432/postgres';
+var testCount = 0;
 
 var data = {
-	key: 'value', 
+	key: 'value',
 	hello: 'world',
 	arr: _.range(5),
 	test_url: '' + process.env.TEST_URL,
-	version :process.version,
-	
+	version: process.version,
+
 };
 
-app.get('/', function(req, res){
-    res.writeHead(200, {"Content-Type": "application/json"});
-    //res.write("<h2>Hello World</h2>");
-    res.write(JSON.stringify(data));
+app.get('/', function (req, res) {
+	res.writeHead(200, { "Content-Type": "application/json" });
+	//res.write("<h2>Hello World</h2>");
+	res.write(JSON.stringify(data));
 	res.end();
 });
 
-app.get('/test', function(req, res){
+app.get('/test', function (req, res) {
 	console.log('test page');
-	testDelay().then(function(rs){
+	testCount ++;
+	testDelay().then(function (rs) {
 
-		res.writeHead(200, {"Content-Type": "text/html"});
-		res.write('<h2>Hello Test -> ' + rs + '</h2>');
+		res.writeHead(200, { "Content-Type": "text/html" });
+		res.write('<h2>Hello Test -> ' + rs + '</h2> count:' + testCount);
 		res.end();
 	});
 });
 
-app.get('/test2', function(req, res){
-	getTest2().then(function(rs){
+app.get('/test2', function (req, res) {
+	getTest2().then(function (rs) {
 
-		res.writeHead(200, {"Content-Type": "text/html"});
-		res.write(rs);
+		res.writeHead(200, { "Content-Type": "text/html" });
+		res.write(JSON.stringify(rs));
 		res.end();
 	});
 });
 
 
-app.get('/db', function(req, res){
-try{
-	
+app.get('/db', function (req, res) {
 	var pg = require('pg');
-	
 	console.log('test db');
-
-	pg.defaults.ssl = true;
-	pg.connect(process.env.DATABASE_URL, function(err, client) {
-	  if (err){
-		 //throw err;
-		 console.log('Error---> ' + err);
-		 res.writeHead(200, {"Content-Type": "text/html"});
-		 res.write('Error---> ' + err);
-		 res.end();
-		 return;
-	  }  
-	  console.log('Connected to postgres! Getting schemas...');
-
-	  client
-		//.query('SELECT table_schema,table_name FROM information_schema.tables;')
-		.query('select * from test1;')
-		.on('row', function(row) {
-		    res.writeHead(200, {"Content-Type": "text/html"});
-			res.write(JSON.stringify(row));
-			console.log(JSON.stringify(row));
+	//pg.defaults.ssl = true;
+	pg.connect(databaseUrl, function (err, client) {
+		if (err) {
+			//throw err;
+			console.log('Error---> ' + err);
+			res.writeHead(200, { "Content-Type": "text/html" });
+			res.write('Error---> ' + err);
 			res.end();
-		});
-	});
-}catch(err){
-	res.writeHead(200, {"Content-Type": "text/html"});
-	res.write(JSON.stringify(err));
-	res.end();
-}
+			return;
+		}
+		console.log('Connected to postgres! Getting schemas...');
 
+		client
+			.query('SELECT table_schema,table_name FROM information_schema.tables;')
+			//.query('select * from test1;')
+			.on('row', function (row) {
+				res.writeHead(200, { "Content-Type": "text/html" });
+				res.write(JSON.stringify(row));
+				console.log(JSON.stringify(row));
+				res.end();
+			});
+	});
 });
 
-function testDelay(){
-	return Promise.delay(1000).then(function(){
+function testDelay() {
+	return Promise.delay(1000).then(function () {
 		return 'delay result';
 	});
 }
 
-function getTest2(){
-var pg = require('pg');
-return new Promise(function(resolve, reject){
+function getTest2() {
+	var pg = require('pg');
+	return new Promise(function (resolve, reject) {
+		var client = new pg.Client(databaseUrl);
 
-	var client = new pg.Client(process.env.DATABASE_URL);
-
-	// connect to our database
-	client.connect(function (err) {
-	  if (err){
-		  reject(err);
-	  } else {
-	  
-	 
-		  client.query('SELECT * from test2', function (err, result) {
-			  if (err){
-				  reject(err);
-			  } else {
-
-			
-				// disconnect the client
-				client.end(function (err) {
-				  if (err){
-					  reject(err);
-				  } else {
-					 resolve(result);  
-				  }
-				});
-			   }
-		  });
-	   }
+		// connect to our database
+		client.connect(function (err) {
+			if (err) {
+				reject(err);
+				return;
+			}
+			client.query('SELECT * from test2 where 1 = 1;', function (err, result) {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(result.rows);
+					// disconnect the client
+					// client.end(function (err) {
+					// 	if (err) {
+					// 		reject(err);
+					// 	} else {
+					// 		resolve(result.rows);
+					// 	}
+					// });
+				}
+			});
+		});
 	});
-});
 }
 
 
-
-server.listen(port, function(){
+server.listen(port, function () {
 	console.log('socket on *:' + port)
 });
 
-process.on('uncaughtException', function(error) {
-    console.error('Caught exception: ' + error);
+process.on('uncaughtException', function (error) {
+	console.error('Caught exception: ' + error);
 });
